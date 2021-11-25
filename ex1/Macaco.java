62eda83bd7c300668360afb8b77233922d38870d
Mariana Gonçalves Rodrigues <mary.judo@gmail.com>
2021-11-25 15:25:48 -0300

    Create Macaco.java

---
+++ ex1/Macaco.java
@@ -0,0 +1,9 @@
+public class Macaco extends Terrestre{
+  public Macaco(String nome, String alimento){
+    super(nome, alimento);
+  }
+
+   public void peso(){
+    System.out.println("3Kg");
+  }
+}
