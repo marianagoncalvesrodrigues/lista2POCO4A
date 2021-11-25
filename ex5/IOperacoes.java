70c3eec165fb538e58e2ac3ad87a43d6b772b52a
Mariana Gonçalves Rodrigues <mary.judo@gmail.com>
2021-11-25 15:17:53 -0300

    Create IOperacoes.java

---
+++ ex5/IOperacoes.java
@@ -0,0 +1,7 @@
+public interface IOperacoes{
+  public abstract void setOperando1(float operando1);
+  public abstract void setOperando2(float operando2);
+  public abstract float getResultado();
+  public abstract String getNome();
+  public abstract int getQuantidade();
+}
