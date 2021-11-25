56ca890d5a0cd744f9a2e9f57139111c1e1879b8
Mariana Gonçalves Rodrigues <mary.judo@gmail.com>
2021-11-25 15:02:17 -0300

    Create Leao.java

---
+++ ex10/Leao.java
@@ -0,0 +1,10 @@
+public class Leao extends Terrestre{
+
+  public Leao(String nome, String alimento){
+    super(nome, alimento);
+  }
+
+  public void peso(){
+    System.out.println("190Kg");
+  }
+}
